1cfa34596ab4902e40c8f1f3a11a21326c8194a5
hava <hava.nagdi@gmail.com>
2023-04-28 14:02:37 -0700

    Simplifed Witch animation

--- luna.gd
+++ luna.gd
@@ -2,39 +2,16 @@ extends CharacterBody2D
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -650.0
-var state = "idle_right"
-var direction_facing = "right"
+@onready var sprite = $AnimatedSprite2D
+@onready var direction = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
-	$AnimatedSprite2D.animation = state
-	$AnimatedSprite2D.play()
+	sprite.play("idle")
 		
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right") and is_on_floor():
-		#if (state != "run_right"):
-			state = "run_right" 
-			direction_facing = "right"
-	elif Input.is_action_pressed("ui_left") and is_on_floor():
-		#if (state != "run_left"):
-			state = "run_left"
-			direction_facing = "left"
-	elif Input.is_action_just_pressed("ui_accept") and direction_facing == "right":
-		#if (state != "jump_right"):
-			state = "jump_right"
-	elif Input.is_action_just_pressed("ui_accept") and direction_facing == "left":
-		if (state != "jump_left"):
-			state = "jump_left"
-	elif (state != "idle_right") and (state != "idle_left"):
-		if (direction_facing == "left"):
-			state = "idle_left"
-		elif (direction_facing == "right"):
-			state = "idle_right"
-	
-	$AnimatedSprite2D.animation = state
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -42,15 +19,29 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
+	update_animation()
+	update_facing_direction()
 
+func update_animation():
+	if direction !=0 and is_on_floor():
+		sprite.play("run") 
+	elif Input.is_action_just_pressed("ui_accept"):
+		sprite.play("jump")
+	else:
+		sprite.play("idle")
+		
+func update_facing_direction():
+	if direction > 0:
+		sprite.flip_h = false
+	elif direction < 0:
+		sprite.flip_h = true
