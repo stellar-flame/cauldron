fe03fbd1c51394dcabcc4a96a291e3d654515f12
hava <hava.nagdi@gmail.com>
2023-10-30 16:50:55 -0700

    LunaAnimate refactor

--- platformer/characters/luna/LunaAnimate.gd
+++ platformer/characters/luna/LunaAnimate.gd
@@ -1,10 +1,6 @@
-extends AnimatedSprite2D
+extends "res://platformer/characters/CharacterAnimation.gd"
 
 signal cast_spell
-
-@onready var animation_blocked = false
-signal died
-var state = "alive"
 
 func update_animation(direction = 0, on_floor = true):
 	if (not animation_blocked):
@@ -19,33 +15,10 @@ func update_animation(direction = 0, on_floor = true):
 		else:
 			play("jump")
 		
-func update_facing_direction(direction = 0):
-	if direction > 0:
-		flip_h = false
-	elif direction < 0:
-		flip_h = true
 
 func take_damage():
 	play("hurt")
 	animation_blocked = true;
-
-func die():
-	state = "dying"
-	
-func _on_animation_finished():
-	if (state == "alive"):
-		animation_blocked = false
-	elif (state == "dying"):
-		play("die")
-		animation_blocked = true;
-		state = "dead"
-	elif (state == "dead"):
-		for i in 4:
-			self_modulate.a = 0.5
-			await get_tree().create_timer(0.3).timeout
-
-		emit_signal("died")
-		
 
 func _on_frame_changed():
 	if (animation == "attack" and frame == 3):
