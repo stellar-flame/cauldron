0f941e8b1b82e990f3c1112e0a5a60ff89844d25
hava <hava.nagdi@gmail.com>
2023-11-29 10:32:14 -0800

    Set Camera Limits for Scene 3

---
+++ platformer/dungeon1/Scene3.gd
@@ -0,0 +1,15 @@
+extends BaseScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super()
+	screen_id = 2
+	camera_limits = { "right" = Global.base_window_size.x + self.position.x, 
+						"left" = Global.base_window_size.x}
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+	
